0d9148574feca4218853c239b39cab65ec2946cf
DoctorDoofen <and.bennett0930@gmail.com>
2024-04-05 01:27:30 -0500

    14 unsolved

--- problems/14-smoothie-machine.js
+++ problems/14-smoothie-machine.js
@@ -21,9 +21,34 @@ let smoothie2 = smoothieMachine("apples", "bananas", "berries");
 console.log(smoothie2("pineapple"));
 // prints "I'm having a smoothie with apples and bananas and berries and pineapple"
 ***********************************************************************/
+function smoothieMachine(...params) {
+  let smoothie = "I'm having a smoothie with";
+  let words = smoothie.split(' ');
 
-// Your code here 
+  return function (...param) {
+    for (let i = 0; i < param.length; i++)
+      if (param[i] !== words.length - 1) {
+        words.push(param[i], ' and ');
+      } else if (words.length - 1 === param.length - 1) {
+        return words;
 
+      }
+    return words
+  }
+}
+
+let smoothie1 = smoothieMachine();
+
+console.log(smoothie1("milk"));
+// prints "I'm having a smoothie with milk"
+console.log(smoothie1("kale", "spinach"));
+// prints "I'm having a smoothie with milk and kale and spinach"
+console.log(smoothie1("honey", "pears", "berries"));
+// prints "I'm having a smoothie with milk and kale and spinach and honey and pears and berries"
+
+let smoothie2 = smoothieMachine("apples", "bananas", "berries");
+console.log(smoothie2("pineapple"));
+// prints "I'm having a smoothie with apples and bananas and berries and pineapple"
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = smoothieMachine;
