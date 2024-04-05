985e3f5e572b360a664c354dc83d9bc306338362
grassella <negrete.graciela@yahoo.com>
2024-04-05 00:23:44 -0500

    11 solved

--- problems/11-lazy-adder.js
+++ problems/11-lazy-adder.js
@@ -22,8 +22,26 @@ AFTER YOU ARE FINISHED WITH THIS PROBLEM, ASK FOR A CODE REVIEW
   fat-arrow function
 ***********************************************************************/
 
-// Your code here 
+function lazyAdder(firstNum) {
 
+  return function(secondNum) {
+    return function(thirdNum) {
+      return firstNum + secondNum + thirdNum;
+    }
+  }
+
+} 
+
+let firstAdd = lazyAdder(1);
+let secondAdd = firstAdd(2);
+let sum = secondAdd(3);
+console.log(sum); // prints 6
+
+
+let func1 = lazyAdder(10);
+let func2 = func1(20);
+let total = func2(3);
+console.log(total); // prints 33
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
