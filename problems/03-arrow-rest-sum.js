0102565a000d6efd2c6ad0035774e40afe933b07
DoctorDoofen <and.bennett0930@gmail.com>
2024-04-04 21:10:59 -0500

    03 solved

--- problems/03-arrow-rest-sum.js
+++ problems/03-arrow-rest-sum.js
@@ -10,9 +10,17 @@ arrowRestSum(3,5,6); // => 14
 arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
 arrowRestSum(0); // => 0
 ***********************************************************************/
+const arrowRestSum = ((...nums) => {
+  let sum = 0;
+  for (let num of nums) {
+    sum += num;
+  }
+  return sum;
+})
 
-// Your code here 
-
+console.log(arrowRestSum(3, 5, 6)); // => 14
+console.log(arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9)); // => 45
+console.log(arrowRestSum(0)); // => 0
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
