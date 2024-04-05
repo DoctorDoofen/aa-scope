4abe1b1a6f00a1921f7a52688a7e3e8a113494e2
DoctorDoofen <and.bennett0930@gmail.com>
2024-04-05 17:25:47 -0500

    19 solved

--- problems/19-coin-collector.js
+++ problems/19-coin-collector.js
@@ -24,9 +24,35 @@ Example 3:
   console.log(threeCoins(10)); // prints [ 25, 5, 10 ]
 
 ***********************************************************************/
+function coinCollector(numCoins) {
+  let totals = [];
 
-// Your code here 
+  return piggyBank = (coin) => {
+    totals.push(coin);
+    if (totals.length === numCoins) {
+      return totals;
 
+    }
+    return piggyBank;
+  }
+}
+
+
+
+// Example 1:
+let oneCoin = coinCollector(1); // returns a function
+console.log(oneCoin(10)); // prints [10]
+
+// Example 2:
+let twoCoins = coinCollector(2); // returns a function
+twoCoins(25); // returns a function
+console.log(twoCoins(10)); // prints [25, 10]
+
+// Example 3:
+let threeCoins = coinCollector(3); // returns a function
+threeCoins(25); // returns a function
+threeCoins(5); // returns a function
+console.log(threeCoins(10)); // prints [ 25, 5, 10 ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
