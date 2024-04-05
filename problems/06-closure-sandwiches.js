e6392fb4970582a04d527dd6a4f08aaad699c4ef
grassella <negrete.graciela@yahoo.com>
2024-04-04 21:40:18 -0500

    06 solved

--- problems/06-closure-sandwiches.js
+++ problems/06-closure-sandwiches.js
@@ -17,8 +17,23 @@ sandwich2("pb") // => "One sandwich with tomato and pb"
 
 ***********************************************************************/
 
-// Your code here 
+function sandwichMaker() {
+  let order = "One sandwich with tomato";
 
+  return function (ingredient) {
+    order += " and " + ingredient;
+    return order;
+  }
+  
+}
+
+let sandwich = sandwichMaker(); // => returns a function
+console.log(sandwich("spinach")) // => "One sandwich with tomato and spinach"
+console.log(sandwich("jelly")) // => "One sandwich with tomato and spinach and jelly"
+console.log(sandwich("bread")) // => "One sandwich with tomato and spinach and jelly and bread"
+
+let sandwich2 = sandwichMaker(); // => returns a function
+console.log(sandwich2("pb")) // => "One sandwich with tomato and pb"
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
